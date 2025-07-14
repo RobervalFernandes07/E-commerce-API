6badd4a9b61d1f47d9dda6ac950ad85ce5f72052
Roberval Fernandes <robervalfernandesdigital@outlook.com>
2025-07-14 20:14:47 -0300

    finalizando cart_item

--- project/app.py
+++ project/app.py
@@ -1,7 +1,7 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin , login_user , LoginManager , logout_user
+from flask_login import UserMixin, login_user, LoginManager, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'teste123'
@@ -13,24 +13,23 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 CORS(app)
 
-
-#Modelagem
-#User (id,username,password,) 
-#Produto (id,name,description,price)
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=True)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
 
-    
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    
-#Authentication
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -40,9 +39,8 @@ def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-
     user = User.query.filter_by(username=username).first()
-    if user and user.verify_password(password):
+    if user and user.password == password:
         login_user(user)
         return jsonify({"message": "Login successful"}), 200
     return jsonify({"error": "Invalid username or password"}), 401
@@ -61,13 +59,12 @@ def add_product():
         product = Product(
             name=data['name'],
             price=data['price'],
-            description=data.get("description",""))
+            description=data.get("description", "")
+        )
         db.session.add(product)
         db.session.commit()
         return jsonify({"message": "Product added successfully"}), 201
-    return jsonify({
-        "error": "Invalid data. Please provide name, price, and description."
-    }), 400
+    return jsonify({"error": "Invalid data. Please provide name, price, and description."}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
 @login_required
@@ -91,13 +88,12 @@ def get_product_details(product_id):
         }), 200
     return jsonify({"error": "Product not found"}), 404
 
-@app.route('/api/products/update/int:<int:product_id>', methods=['PUT'])
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
 @login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
         return jsonify({"error": "Product not found"}), 404
-    
     data = request.json
     if 'name' in data:
         product.name = data['name']
@@ -105,7 +101,6 @@ def update_product(product_id):
         product.price = data['price']
     if 'description' in data:
         product.description = data['description']
-    
     db.session.commit()
     return jsonify({"message": "Product updated successfully"}), 200
 
@@ -118,7 +113,7 @@ def get_products():
         "price": product.price,
         "description": product.description
     } for product in products]), 200
-    
+
 
 
 if __name__ == '__main__':
